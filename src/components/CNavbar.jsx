2134edff474ca3a966995d3a5055f316ae737fa1
Matteo Furgani <151537051+MatteoFurgani@users.noreply.github.com>
2024-02-21 16:56:49 +0100

    modifica

--- src/components/CNavbar.jsx
+++ src/components/CNavbar.jsx
@@ -7,6 +7,8 @@ import {
   InputGroup,
   Dropdown,
   Badge,
+  Modal,
+  Button,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
@@ -20,202 +22,265 @@ import {
   PersonCircle,
 } from "react-bootstrap-icons";
 import Logo from "/logoIn.png";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { PERSONAL_PROFILE } from "../redux/actions";
 
 const CNavbar = () => {
-  return (
-    <Navbar bg="white" expand="lg" className="shadow-sm fixed-top mb-5">
-      <Container
-        fluid
-        style={{
-          paddingLeft: "160px",
-          paddingRight: "100px",
-          position: "relative",
-          paddingTop: "0px",
-          paddingBottom: "0px",
-          marginTop: "-2px",
-          marginBottom: "-10px",
-        }}
-      >
-        <Navbar.Brand href="#home">
-          <img
-            src={Logo}
-            width="34"
-            height="34"
-            className="d-inline-block align-top"
-            alt="LinkedIn logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Form className="me-3">
-            <InputGroup>
-              <FormControl
-                type="search"
-                placeholder="Cerca"
-                aria-label="Cerca"
-                className="border-0 bg-light"
-                style={{ paddingRight: "50px", boxShadow: "none" }}
-              />
-              <InputGroup.Text
-                style={{
-                  position: "absolute",
-                  right: "10px",
-                  top: "50%",
-                  transform: "translateY(-50%)",
-                  zIndex: "5",
-                  background: "none",
-                  border: "none",
-                }}
-              >
-                <Search size={20} />
-              </InputGroup.Text>
-            </InputGroup>
-          </Form>
-          <Nav
-            className="me-auto justify-content-center text-center"
-            style={{ flexGrow: 1 }}
-          >
-            <Nav.Item style={{ marginLeft: "24px" }}>
-              <Nav.Link href="#home">
-                <HouseDoorFill size={20} />
-                <div>Home</div>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item style={{ marginLeft: "24px" }}>
-              <Nav.Link href="#rete">
-                <PeopleFill size={20} />
-                <div>Rete</div>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item style={{ marginLeft: "24px" }}>
-              <Nav.Link href="#lavoro">
-                <SuitcaseFill size={20} />
-                <div>Lavoro</div>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item style={{ marginLeft: "24px" }}>
-              <Nav.Link href="#messaggi">
-                <ChatDots size={20} />
-                <div>Messaggi</div>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item style={{ marginLeft: "24px", position: "relative" }}>
-              <Badge
-                bg="danger"
-                className="position-absolute translate-middle badge-custom"
-                style={{ top: "8px", right: "7px" }}
-              >
-                4
-              </Badge>{" "}
-              <Nav.Link href="#notifiche">
-                <BellFill size={20} />
-                <div>Notifiche</div>
-              </Nav.Link>
-            </Nav.Item>
+  const [show, setShow] = useState(false);
+  const [newProfileName, setNewProfileName] = useState("");
+  const dispatch = useDispatch();
 
-            <Nav.Item style={{ marginLeft: "24px" }}>
-              <Dropdown align="end">
-                <Dropdown.Toggle
-                  variant="transparent"
-                  id="dropdown-profile"
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const changeProfile = () => {
+    if (newProfileName.trim() !== "") {
+      dispatch({
+        type: PERSONAL_PROFILE,
+        payload: {
+          name: newProfileName,
+          surname: "",
+          title: "",
+          area: "",
+          image: "",
+        },
+      });
+      setShow(false);
+    } else {
+      alert("Inserisci un nome valido per il profilo.");
+    }
+  };
+
+  return (
+    <>
+      <Navbar bg="white" expand="lg" className="shadow-sm fixed-top mb-5">
+        <Container
+          fluid
+          style={{
+            paddingLeft: "160px",
+            paddingRight: "100px",
+            position: "relative",
+            paddingTop: "0px",
+            paddingBottom: "0px",
+            marginTop: "-2px",
+            marginBottom: "-10px",
+          }}
+        >
+          <Navbar.Brand href="#home">
+            <img
+              src={Logo}
+              width="34"
+              height="34"
+              className="d-inline-block align-top"
+              alt="LinkedIn logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Form className="me-3">
+              <InputGroup>
+                <FormControl
+                  type="search"
+                  placeholder="Cerca"
+                  aria-label="Cerca"
+                  className="border-0 bg-light"
+                  style={{ paddingRight: "50px", boxShadow: "none" }}
+                />
+                <InputGroup.Text
                   style={{
-                    boxShadow: "none",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    padding: "0",
+                    position: "absolute",
+                    right: "10px",
+                    top: "50%",
+                    transform: "translateY(-50%)",
+                    zIndex: "5",
+                    background: "none",
                     border: "none",
-                    backgroundColor: "transparent",
-                    cursor: "pointer",
                   }}
                 >
-                  <img
-                    src="/path-to-default-image"
+                  <Search size={20} />
+                </InputGroup.Text>
+              </InputGroup>
+            </Form>
+            <Nav
+              className="me-auto justify-content-center text-center"
+              style={{ flexGrow: 1 }}
+            >
+              <Nav.Item style={{ marginLeft: "24px" }}>
+                <Nav.Link href="#home">
+                  <HouseDoorFill size={20} />
+                  <div>Home</div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item style={{ marginLeft: "24px" }}>
+                <Nav.Link href="#rete">
+                  <PeopleFill size={20} />
+                  <div>Rete</div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item style={{ marginLeft: "24px" }}>
+                <Nav.Link href="#lavoro">
+                  <SuitcaseFill size={20} />
+                  <div>Lavoro</div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item style={{ marginLeft: "24px" }}>
+                <Nav.Link href="#messaggi">
+                  <ChatDots size={20} />
+                  <div>Messaggi</div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item style={{ marginLeft: "24px", position: "relative" }}>
+                <Badge
+                  bg="danger"
+                  className="position-absolute translate-middle badge-custom"
+                  style={{ top: "8px", right: "7px" }}
+                >
+                  4
+                </Badge>{" "}
+                <Nav.Link href="#notifiche">
+                  <BellFill size={20} />
+                  <div>Notifiche</div>
+                </Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item style={{ marginLeft: "24px" }}>
+                <Dropdown align="end">
+                  <Dropdown.Toggle
+                    variant="transparent"
+                    id="dropdown-profile"
                     style={{
-                      width: "34px",
-                      height: "34px",
-                      borderRadius: "50%",
-                      objectFit: "cover",
-                      marginTop: "8px",
+                      boxShadow: "none",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      padding: "0",
+                      border: "none",
+                      backgroundColor: "transparent",
+                      cursor: "pointer",
                     }}
-                    alt="Profilo"
-                  />
-                </Dropdown.Toggle>
-                <Dropdown.Menu
-                  style={{
-                    maxWidth: "250px",
-                    padding: "0",
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                  }}
-                >
-                  <Dropdown.Item
-                    href="#action/3.1"
-                    style={{ fontWeight: "bold" }}
                   >
-                    Nome - Ruolo
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="#action/3.2">
-                    Impostazioni e privacy
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#action/3.3">Guida</Dropdown.Item>
-                  <Dropdown.Item href="#action/3.4">Lingua</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="#action/3.5">Gestisci</Dropdown.Item>
-                  <Dropdown.Item href="#action/3.6">
-                    Post e attività
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    href="#action/3.7"
+                    <img
+                      src="/path-to-default-image"
+                      style={{
+                        width: "34px",
+                        height: "34px",
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                        marginTop: "8px",
+                      }}
+                      alt="Profilo"
+                    />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu
                     style={{
+                      maxWidth: "250px",
+                      padding: "0",
                       whiteSpace: "nowrap",
                       overflow: "hidden",
                       textOverflow: "ellipsis",
                     }}
                   >
-                    Account per la pubblicazione di offerte di lavoro
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#action/3.8">Esci</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Nav.Item>
-            {/* Div per la linea di separazione */}
-            <Nav.Item
-              style={{
-                alignSelf: "stretch",
-                width: "1px",
-                backgroundColor: "#dcdcdc",
-                marginTop: "-2px",
-                marginBottom: "5px",
-                marginLeft: "25px",
-                marginRight: "-23px",
-              }}
-            />
+                    <Dropdown.Item
+                      href="#action/3.1"
+                      style={{ fontWeight: "bold" }}
+                    >
+                      Nome - Ruolo
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#action/3.2">
+                      Impostazioni e privacy
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#action/3.3">Guida</Dropdown.Item>
+                    <Dropdown.Item href="#action/3.4">Lingua</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item href="#action/3.5">Gestisci</Dropdown.Item>
+                    <Dropdown.Item href="#action/3.6">
+                      Post e attività
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                      href="#action/3.7"
+                      style={{
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      Account per la pubblicazione di offerte di lavoro
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#action/3.8" onClick={handleShow}>
+                      Cambia utente
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Nav.Item>
+              {/* Div per la linea di separazione */}
+              <Nav.Item
+                style={{
+                  alignSelf: "stretch",
+                  width: "1px",
+                  backgroundColor: "#dcdcdc",
+                  marginTop: "-2px",
+                  marginBottom: "5px",
+                  marginLeft: "25px",
+                  marginRight: "-23px",
+                }}
+              />
 
-            <Nav.Item style={{ marginLeft: "24px" }}>
-              <Dropdown align="end"></Dropdown>
-            </Nav.Item>
-            {/* fine Div per la linea di separazione */}
-            <Nav.Item style={{ marginLeft: "24px" }}>
-              <Nav.Link href="#altro">
-                <Grid3x3GapFill size={20} />
-                <div>Per le aziende</div>
+              <Nav.Item style={{ marginLeft: "24px" }}>
+                <Dropdown align="end"></Dropdown>
+              </Nav.Item>
+              {/* fine Div per la linea di separazione */}
+              <Nav.Item style={{ marginLeft: "24px" }}>
+                <Nav.Link href="#altro">
+                  <Grid3x3GapFill size={20} />
+                  <div>Per le aziende</div>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Nav className="justify-content-end">
+              <Nav.Link href="#premium" style={{ paddingRight: "40px" }}>
+                <div className="text-primary" style={{ cursor: "pointer" }}>
+                  Prova Premium
+                  <br /> per 0 EUR
+                </div>
               </Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <Nav className="justify-content-end">
-            <Nav.Link href="#premium" style={{ paddingRight: "40px" }}>
-              <div className="text-primary" style={{ cursor: "pointer" }}>
-                Prova Premium
-                <br /> per 0 EUR
-              </div>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {" "}
+            <h5>Cambia Utente</h5>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="pt-0">
+          <p className=" opacity-75 ">* indica che è obbligatorio</p>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label className=" opacity-75 ">Nome*</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=""
+                value={newProfileName}
+                onChange={(e) => setNewProfileName(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={handleClose}
+            className=" rounded-5 px-3"
+          >
+            Cambia
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
