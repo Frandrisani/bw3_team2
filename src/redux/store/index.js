0160a18334cef9b1e0c35e4fd7aba832c91c31f4
Simone Manca <simone.manca.98@gmail.com>
2024-02-21 11:23:24 +0100

    changes

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import profileReducer from "../reducers/profile";
+import postsReducer from "../reducers/postsReducer"; // Per homepage
 
 const globalReducer = combineReducers({
   profile: profileReducer,
+  postsReducer: postsReducer, // per homepage
 });
 
 const store = configureStore({
